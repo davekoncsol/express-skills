1244af687c19160fcecb80764f694e1daa82f60c
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-08-15 14:46:54 -0700

    updated

---
+++ models/skill.js
@@ -0,0 +1,14 @@
+const skills = [
+    {skill: 'Feed Dogs', done: true},
+    {skill: 'Learn Express', done: false},
+    {skill: 'Buy Milk', done: false}
+  ];
+  
+  module.exports = {
+    getAll
+    
+  };
+
+  function getAll(){
+      return skills;
+  }
