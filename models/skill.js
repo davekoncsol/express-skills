3f44f25f4a3c9254b43b3c98da3aea5e602b4b91
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-08-16 15:16:01 -0700

    finished dev skills node lab

--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,38 @@
+
+
 const skills = [
-    {skill: 'Feed Dogs', done: true},
-    {skill: 'Learn Express', done: false},
-    {skill: 'Buy Milk', done: false}
+    {skill: 'CSS', done: true},
+    {skill: 'JavaScript', done: false},
+    {skill: 'HTML', done: false}
   ];
   
   module.exports = {
-    getAll
+    getAll,
+    getOne,
+    create,
+    deleteOne,
+    update
     
   };
 
-  function getAll(){
-      return skills;
+
+
+function getOne(id) {
+  return skills[id];
+}
+
+function getAll() {
+  return skills;
+}
+
+function create(skill){
+    skills.push(skill);
+}
+
+function deleteOne(id){
+    skills.splice(id, 1);
+}
+
+function update(id, skill){
+  skills[id] = skill;
   }
